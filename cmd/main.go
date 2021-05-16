f8749e4c7801cc8054bfefdbedc710a97b371a83
Ravshan Sharifov <sharifovrav@gmail.com>
2021-05-16 18:09:24 +0500

    modif server and adding main.go

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/rgsgit/http/pkg/server"
+)
+
+
+
+func main() {
+	host := "0.0.0.0"
+	port := "9999"
+	
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute(host string, port string) (err error) {
+	srv := server.NewServer(net.JoinHostPort(host, port))
+
+	srv.Register("/", func(conn net.Conn) {
+		body := "Welcome to our web-site"
+
+		_, err = conn.Write([]byte(srv.Response(body)))
+		if err != nil {
+			log.Print(err)
+		}
+	})
+
+	srv.Register("/about", func(conn net.Conn) {
+		body := "About Golang Academy"
+
+		_, err = conn.Write([]byte(srv.Response(body)))
+		if err != nil {
+			log.Print(err)
+		}
+	})
+	return srv.Start()
+}
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,7 +3,14 @@ package server
 import (
 	"net"
 	"sync"
+	"strconv"
+	"strings"
+	"bytes"
+	"io"
+	"log"
 )
+
+const lineCRLF string = "\r\n"
 
 type HandlerFunc func(con net.Conn)
 
@@ -25,5 +32,87 @@ func (s *Server) Register(path string, handler HandlerFunc)  {
 
 func (s *Server) Start() error{
 
-	return nil
+	listener, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+				return
+			}
+			log.Print(cerr)
+		}
+	}()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+//handle - handles the connection
+func (s *Server) handle(conn net.Conn) {
+
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err == io.EOF {
+		log.Printf("Error EOF: %s", buf[:n])
+		return
+	}
+	if err != nil {
+		log.Println("Error not nil after reading", err)
+		return
+	}
+
+	//Parsing...
+	data := buf[:n]
+	requestLineDelim := []byte{'\r', '\n'}
+	requestLineEnd := bytes.Index(data, requestLineDelim)
+
+	if requestLineEnd == -1 {
+		log.Print("requestLineEndErr: ", requestLineEnd)
+		return
+	}
+
+	requestLine := string(data[:requestLineEnd])
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		log.Print("Parts: ", parts)
+		return
+	}
+
+	path, version := parts[1], parts[2]
+
+	if version != "HTTP/1.1" {
+		log.Print("Version Not HTTP/1.1: ", version)
+		return
+	}
+
+	s.mu.RLock()
+	if handler, ok := s.handlers[path]; ok {
+		s.mu.RUnlock()
+		handler(conn)
+	}
+}
+
+
+func (s *Server) Response(body string) string {
+	return "HTTP/1.1 200 OK\r\n" +
+		"Content-Length: " + strconv.Itoa(len(body)) + lineCRLF +
+		"Content-Type: text/html\r\n" +
+		"Connection: close\r\n" +
+		lineCRLF + body
 }
